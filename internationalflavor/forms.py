6c60c909e9509213550295eeaed811e2b9b88e4a
Ralph Broenink <ralph@ralphbroenink.net>
2017-09-11 18:42:12 +0200

    Attempt to fix the flake8 errors

--- internationalflavor/forms.py
+++ internationalflavor/forms.py
@@ -47,11 +47,22 @@ def _compare_str(value):
 
 
 def _option_label_getter(item):
+    # For <=1.10
     # Sort optgroups above other items by putting them in a tuple
     if isinstance(item[1], (list, tuple)):
         return 0, _compare_str(item[0])
     else:
         return 1, _compare_str(item[1])
+
+
+def _ctxt_optgroup_label_getter(item):
+    # For >=1.11
+    # we sort optgroups by sorting None below their unicode comparison (using a tuple for that)
+    # because optgroup None may occur more than once, we sort these by the label of the first item in the optgroup
+    if item[0] is None:
+        return 1, _compare_str(item[1][0]['label'])
+    else:
+        return 0, _compare_str(item[0])
 
 
 class SortedSelect(forms.Select):
@@ -61,13 +72,10 @@ class SortedSelect(forms.Select):
         """Overrides the rendering of options starting Django 1.11"""
         context = super(forms.Select, self).get_context(name, value, attrs)
         # we sort options in optgroups by their unicode comparison
-        # we sort optgroups by sorting None below their unicode comparison (using a tuple for that)
-        # because optgroup None may occur more than once, we sort these by the label of the first item in the optgroup
+        # optgroups themselves are ordered by their label (if any) or the first item of the group
         context['widget']['optgroups'] = sorted([(a, sorted(choices, key=lambda o: _compare_str(o['label'])), c)
                                                  for a, choices, c in context['widget']['optgroups']],
-                                                key=lambda og: (1, _compare_str(og[1][0]['label']))
-                                                               if og[0] is None else
-                                                               (0, _compare_str(og[0])))
+                                                key=_ctxt_optgroup_label_getter)
         return context
 
     def render_options(self, *args):
